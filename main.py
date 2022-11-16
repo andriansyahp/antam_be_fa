516b27939911a668a2cea887d0422908ff2bcd34
Andriansyah Putra <muhammad.andriansyah@ui.ac.id>
2022-11-16 13:04:49 +0700

    trying heroku

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.utils import ChromeType
 from time import sleep
 from urllib.parse import quote
@@ -48,7 +50,8 @@ async def create_upload_file(file: UploadFile = File(...)):
     options = Options()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     options.add_argument("--profile-directory=Default")
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    # driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
     # driver = webdriver.Chrome(ChromeDriverManager(chrome_type=ChromeType.GOOGLE).install(), options=options)
     delay = 30
     driver.get('https://web.whatsapp.com')
